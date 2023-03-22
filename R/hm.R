524c25039fd4c61f19ba69058c38d96c3eddc00a
Rubens-Moreno <rubensmsimoes@hotmail.com>
2023-03-22 11:04:22 -0300

    hm Funcion

--- R/hm.R
+++ R/hm.R
@@ -1,3 +1,20 @@
+#' Função hm
+#'
+#' @param hr Altura de recalque
+#' @param hs Altura de sucção
+#' @param dr_com Diâmetro comercial
+#' @param q Vazão
+#' @param lr Comprimento do recalque
+#' @param per
+#' @param rc Coficiente de rugosidade
+#' @param ds_com
+#' @param ls
+#' @param pes
+#'
+#' @return
+#' @export
+#'
+#' @examples
 hm <-  function(hr,hs,dr_com,q,lr,per,rc,ds_com,ls,pes){
   hm <- hf::head_loss(dr_com, q/3600, lr+per*dr_com, rc)+hf::head_loss(ds_com, q/3600, ls+pes*ds_com, rc)+hr+hs
 
