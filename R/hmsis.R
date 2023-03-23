daaca4007e429ae61e3ed5e62f8714e354469dd8
Rubens-Moreno <rubensmsimoes@hotmail.com>
2023-03-23 16:42:53 -0300

    atualizou o pacote

--- R/hmsis.R
+++ R/hmsis.R
@@ -18,3 +18,18 @@ hmsis <- function(hr,hs,dr_com,q,lr,per,rc,ds_com,ls,pes){
   return(data.frame(q = qc, hm = hmsis_v))
 
 }
+
+
+
+#eixos do data frame
+qc<-c(0.3*q,0.6*q,q,1.5*q,2*q)
+hmsis_v[i]=hm(hr,hs,dr_com,qc[i],lr,per,rc,ds_com,ls,pes)
+
+#tabela (Data Frame)
+curva_sistema<-data.frame(q = qc, hm = hmsis)
+curva_sistema
+
+#ajuste polinomial da curva_sistema
+Model<-lm(formula = hm ~ q + I(q^2), data = curva_sistema)
+Model
+
---
+++ R/plot_sistema.R
@@ -0,0 +1,7 @@
+#plot da curva
+plot(curva_sistema,
+     type="b",
+     lwd=2,
+     xlab="Vazao - Q(m3/h)",
+     ylab="Altura manometrica - Hm (mca)",
+     main="4. Curva Caracteristica do Sistema")
