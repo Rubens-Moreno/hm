7a2ff741e7f34405d711172a0e69f8ca04d5d2dc
Rubens-Moreno <rubensmsimoes@hotmail.com>
2023-04-13 14:54:25 -0300

    updated

--- R/intersect.R
+++ R/intersect.R
@@ -1,9 +1,8 @@
-# #
 # polyroot(curva_sistema[[2]]$coefficients - curva_bomba[[2]]$coefficients)
-# #
-# # # 12,94 de vazao
-# #
-# # #tentativa 1
+#
+# 12,94 de vazao
+#
+#tentativa 1
 # root_dif <- polyroot(curva_sistema[[2]]$coefficients - curva_bomba[[2]]$coefficients)
 #
 # print(root_dif)
@@ -28,31 +27,3 @@
 #
 # sis_line(rt$root)
 # pump_line(rt$root)
-#
- # polyroot(curva_sistema[[2]]$coefficients - curva_bomba[[2]]$coefficients)
-
-# 12,94 de vazao
-
-#
-# # sis_line <- function(model_sis)
-# # {
-# #   model_sis$coefficients[3] * x^2 + model_sis$coefficients[2] * x + model_sis$coefficients[1]
-# # }
-# #
-# # pump_line <- function(model_bom)
-# # {
-# #   model_bom$coefficients[3]  * x^2 + model_bom$coefficients[2] * x + model_bom$coefficients[1]
-# # }
-# #
-# # rt <- uniroot(function(x)  sis_line(x) - pump_line(x)  , interval=c(0,80), tol=1e-8)
-# # all.equal(sis_line(rt$root), pump_line(rt$root))
-# #
-# # rt$root
-# #
-# # sis_line(rt$root)
-# # pump_line(rt$root)
-# #
-# >>>>>>> 8585e61f06ac6e236c1f5b756a6683916d189eaf
-#
-# curva_sistema[[2]]$coefficients
-# curva_bomba[[2]]$coefficients
