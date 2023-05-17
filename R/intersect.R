00b184c8b8dd20b33f21a5311f35cbe2a0c7c7f7
Rubens-Moreno <rubensmsimoes@hotmail.com>
2023-05-17 00:25:28 -0300

    updated

--- R/intersect.R
+++ R/intersect.R
@@ -1,55 +1,53 @@
-#' intersect point between sis and pump (sis function)
+#' #' intersect point between sis and pump (sis function)
+#' #'
+#' #' @param x System coefficients
+#' #' @param system_curve System curve
+#' #'
+#' #' @return One polynomial
+#' #' @export
+#' #'
+#' #' @examples
+#' #' @examples
+#' #' system_curve <- hmsis(
+#' #'  hr = 3, hs = 1, dr_com = 0.0352, q = 6.8, lr = 18, per = 148,
+#' #'  rc = 0.0001, ds_com = 0.0534, ls = 1, pes = 305
+#' #' )
+#' func1 <- function(x, system_curve)
+#' {
+#'   coef(system_curve[[2]])[[3]] * x^2 + coef(system_curve[[2]])[[2]] * x + coef(system_curve[[2]])[[1]]
+#' }
 #'
-#' @param x System coefficients
-#' @param system_curve System curve
-#'
-#' @return One polynomial
-#' @export
-#'
-#' @examples
-#'system_curve <- function(hmsis)
-#'{
-#'  (hmsis(hr = 3, hs = 1, dr_com = 0.0352, q = 6.8, lr = 18, per = 148,
-#'         rc = 0.0001, ds_com = 0.0534, ls = 1, pes = 305))
-#'}
+#' system_curve <- hmsis(
+#'   hr = 3, hs = 1, dr_com = 0.0352, q = 6.8, lr = 18, per = 148,
+#'   rc = 0.0001, ds_com = 0.0534, ls = 1, pes = 305
 #' )
-func1 <- function(x, system_curve)
-{
-  coef(system_curve[[2]])[[3]] * x^2 + coef(system_curve[[2]])[[2]] * x + coef(system_curve[[2]])[[1]]
-}
-
-
-
-
-
-system_curve <- function(hmsis)
-  {
-  (hmsis(hr = 3, hs = 1, dr_com = 0.0352, q = 6.8, lr = 18, per = 148,
-  rc = 0.0001, ds_com = 0.0534, ls = 1, pes = 305))
-}
-
-
-
-
-
-#' pump function
 #'
-#' @param x Pump coefficients
-#' @param pump_curve Pump curve
+#' #' pump function
+#' #'
+#' #' @param x Pump coefficients
+#' #' @param pump_curve Pump curve
+#' #'
+#' #' @return One polynomial
+#' #' @export
+#' #'
+#' #' @examples
+#' #' q_pump <- c(19.2, 18.2, 17.2, 16.0, 13.3, 9.9)
+#' #' hm_pump <- c(9, 10, 11, 12, 14, 16)
+#' #' pump_curve <- pumpsis(q_pump, hm_pump)
+#' func2 <- function(x, pump_curve)
+#' {
+#'   coef(pump_curve[[2]])[[3]] * x^2 + coef(pump_curve[[2]])[[2]] * x + coef(pump_curve[[2]])[[1]]
+#' }
 #'
-#' @return One polynomial
-#' @export
+#' #uniroot
+#' rt <- uniroot(function(x, system_curve, pump_curve)  func1(x, system_curve) - func2(x, pump_curve)  , interval=c(0,80), tol=1e-8)
+#' #uniroot check
+#' all.equal(func1(rt$root), func2(rt$root))
 #'
-#' @examples
-#' q_pump <- c(19.2, 18.2, 17.2, 16.0, 13.3, 9.9)
-#' hm_pump <- c(9, 10, 11, 12, 14, 16)
-#' pump_curve <- pumpsis(q_pump, hm_pump)
-func2 <- function(x, pump_curve)
-{
-  coef(pump_curve[[2]])[[3]] * x^2 + coef(pump_curve[[2]])[[2]] * x + coef(pump_curve[[2]])[[1]]
-}
 
-#uniroot
-rt <- uniroot(function(x, system_curve, pump_curve)  func1(x, system_curve) - func2(x, pump_curve)  , interval=c(0,80), tol=1e-8)
-#uniroot check
-all.equal(func1(rt$root), func2(rt$root))
+
+#NEW CODE
+
+root_dif <- polyroot(system_curve[[2]]$coefficients - pump_curve[[2]]$coefficients)
+
+print(root_dif)
--- R/plot_intersect.R
+++ R/plot_intersect.R
@@ -1,33 +1,62 @@
-#' Intersection of system and pump curves
-#'
-#' @param df Data.frame
-#'
-#' @return Plot
-#' @export
-#'
-#' @examples
-#' curves <- pumpsis(func1(x), func2(x))
-#' plot.intersect(curves)
-plot.intersect <- function(df) {
-  x <- seq(0, 20, length=100)
-  plot(x,
-       func1(x),
-       col="blue",
-       type="l",
-       lwd=3,
-       xlab="Vazao - Q(m3/h)",
-       ylab="Altura manometrica - Hm (mca)",
-       main="6. Intercessao")
-  lines(x, func2(x), col="red", lwd="3", type="l")
-  points(rt$root, func1(rt$root), pch=16, cex=2)
+#PLOT
+func1 <- function(x, system_curve) {
+  coef(system_curve[[2]])[[3]] * x^2 + coef(system_curve[[2]])[[2]] * x + coef(system_curve[[2]])[[1]]
 }
 
-#func1(rt$root)
-rt$root
-#conferindo
-func1(rt$root)
-func2(rt$root)
+func2 <- function(x, pump_curve) {
+  approx(pump_curve[[2]]$x, pump_curve[[2]]$y, xout = x)$y
+}
 
+x <- seq(0, 20, length.out = 100)
+
+y_system <- func1(x, system_curve)
+y_pump <- func2(x, pump_curve)
+
+# Plotagem do gráfico
+plot(x, y_system, type = "l", lwd = 3, col = "blue", xlab = "x", ylab = "y", ylim = range(y_system, y_pump))
+lines(x, y_pump[[2]], type = "l", lwd = 3, col = "red")
+abline(h = 0, lwd = 1, col = "black")  # Linha horizontal em y = 0
+
+# Destaque na raiz encontrada
+points(Re(root_dif), Im(root_dif), pch = 16, col = "green", cex = 2)
+
+# Legenda
+legend("topleft", legend = c("system_curve", "pump_curve", "Intersection"), col = c("blue", "red", "green"), lwd = 2, pch = c("-", "-", 16), cex = 0.8)
+
+
+
+
+
+#' #' Intersection of system and pump curves
+#' #'
+#' #' @param df Data.frame
+#' #'
+#' #' @return Plot
+#' #' @export
+#' #'
+#' #' @examples
+#' #' curves <- pumpsis(func1(x), func2(x))
+#' #' plot.intersect(curves)
+#' plot.intersect <- function(df) {
+#'   x <- seq(0, 20, length=100)
+#'   plot(x,
+#'        func1(x),
+#'        col="blue",
+#'        type="l",
+#'        lwd=3,
+#'        xlab="Vazao - Q(m3/h)",
+#'        ylab="Altura manometrica - Hm (mca)",
+#'        main="6. Intercessao")
+#'   lines(x, func2(x), col="red", lwd="3", type="l")
+#'   points(rt$root, func1(rt$root), pch=16, cex=2)
+#' }
+#'
+#' #func1(rt$root)
+#' rt$root
+#' #conferindo
+#' func1(rt$root)
+#' func2(rt$root)
+#'
 
 
 
